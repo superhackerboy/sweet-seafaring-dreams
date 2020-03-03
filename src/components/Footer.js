21bb4b8af0e3bb93cbfce874624f9ca5b74079d0
superhackerboy <mark@superhackerman.com>
2020-03-03 15:18:45 -0500

    Finished mobile CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 const Footer = () => (
-  <div className="flex justify-center -mt-6">
+  <div className="flex justify-center -mt-8">
     <a
       href="https://www.linkedin.com/in/superhackerman/"
       target="_blank"
       rel="noopener noreferrer"
       className="pr-4"
     >
-      <i className="fab fa-linkedin-in" />
+      <i
+        className="fab fa-linkedin-in Class	Size
+fa-lg"
+      />
     </a>
 
     <a
@@ -16,7 +19,10 @@ const Footer = () => (
       target="_blank"
       rel="noopener noreferrer"
     >
-      <i className="fab fa-github" />
+      <i
+        className="fab fa-github Class	Size
+fa-lg"
+      />
     </a>
   </div>
 );
