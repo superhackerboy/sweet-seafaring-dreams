c794ec6895b7e247d705191e2eda6b596e2c671d
superhackerboy <mark@superhackerman.com>
2020-03-03 16:03:53 -0500

    Updates

---
+++ precache-manifest.dd71a707685c110e4bfc63ab4626b38d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62be41f3ef312b9b8de4c37c7f63e87f",
+    "url": "/sweet-seafaring-dreams/index.html"
+  },
+  {
+    "revision": "22ec95ac762d435fc1b6",
+    "url": "/sweet-seafaring-dreams/static/css/main.2cef6657.chunk.css"
+  },
+  {
+    "revision": "d6f5a6c17ab3d501bd3e",
+    "url": "/sweet-seafaring-dreams/static/js/2.7d20a2de.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sweet-seafaring-dreams/static/js/2.7d20a2de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ec95ac762d435fc1b6",
+    "url": "/sweet-seafaring-dreams/static/js/main.f368a809.chunk.js"
+  },
+  {
+    "revision": "cbb7021a0f9f8a41766d",
+    "url": "/sweet-seafaring-dreams/static/js/runtime-main.f081f3ab.js"
+  }
+]);
