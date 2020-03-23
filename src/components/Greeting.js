56c58084167819d44a1727b7b6cfd90dabd1e3fb
Mark Harless <mark@superhackerman.com>
2020-03-23 12:32:22 -0400

    Starting fresh

--- src/components/Greeting.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Greeting = props => <p>{props.message}</p>;
-
-export default Greeting;
--- src/components/WeatherDescription.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const WeatherDescription = props => (
-  <p className="weather-condition mb-4 text-sm">{props.description}</p>
-);
-
-export default WeatherDescription;
